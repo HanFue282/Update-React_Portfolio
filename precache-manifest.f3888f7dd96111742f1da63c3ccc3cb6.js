b4cc6169463f8b6f977b505ba74b30aee8169c4c
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-04-03 09:36:11 -0500

    Updates

---
+++ precache-manifest.f3888f7dd96111742f1da63c3ccc3cb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979f25a5eaae2e7e0e1964a0949140fe",
+    "url": "/Update-React_Portfolio/index.html"
+  },
+  {
+    "revision": "b1d2c0f50a9b2a95eb3a",
+    "url": "/Update-React_Portfolio/static/css/main.af03bc79.chunk.css"
+  },
+  {
+    "revision": "984fea1b0d469f0a50f5",
+    "url": "/Update-React_Portfolio/static/js/2.195bbd76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Update-React_Portfolio/static/js/2.195bbd76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d2c0f50a9b2a95eb3a",
+    "url": "/Update-React_Portfolio/static/js/main.a3c18612.chunk.js"
+  },
+  {
+    "revision": "d0f20e0d528d0b9bf67a",
+    "url": "/Update-React_Portfolio/static/js/runtime-main.39e75199.js"
+  },
+  {
+    "revision": "d63ec1a816d8a1984d979a551dbe0552",
+    "url": "/Update-React_Portfolio/static/media/IMG_3268.d63ec1a8.jpg"
+  },
+  {
+    "revision": "1620d995f8038c97ac0fe6305383cacc",
+    "url": "/Update-React_Portfolio/static/media/github-logo.1620d995.png"
+  }
+]);
